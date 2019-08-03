661b5730e27ef4f147fbeb7ab8f2405717e28d42
Aslam Hadi Harsono <aslamhadi@gmail.com>
2019-08-03 21:02:52 +0700

    update wsl provision

--- provision-wsl.sh
+++ provision-wsl.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 # this assumes the public key is created and uploaded to github
 WORKSPACE=$HOME/projects
 DOTFILES_DIR=$WORKSPACE/dotfiles
 DOTFILES_REPO=git@github.com:aslamhadi/dotfiles.git
+DIRCOLORS_DIR=$WORKSPACE/dircolors-solarized
+DIRCOLORS_REPO=https://github.com/seebi/dircolors-solarized
 NVIM_CONFIG=$HOME/.config/nvim
 
 echo "dotfiles"
 mkdir -p $DOTFILES_DIR
 git clone $DOTFILES_REPO $DOTFILES_DIR
-#ln -s $DOTFILES_DIR/gitconfig $HOME/.gitconfig
-#ln -s $DOTFILES_DIR/gitignore_global $HOME/.gitignore_global
+ln -s $DOTFILES_DIR/gitconfig $HOME/.gitconfig
+ln -s $DOTFILES_DIR/gitignore_global $HOME/.gitignore_global
 ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES_DIR/bashrc $HOME/.bash_aliases
 
-echo "oh my zsh"
-#sudo apt-get update -y
-#sudo apt-get install -y zsh 
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#ln -s $DOTFILES_DIR/oh-my-zsh/config.zsh $HOME/.oh-my-zsh/custom/config.zsh
-#sudo chsh -s `which zsh` $USER
+echo "color scheme"
+git clone $DIRCOLORS_REPO $DIRCOLORS_DIR
+
+# echo "oh my zsh"
+# sudo apt-get update -y
+# sudo apt-get install -y zsh 
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# ln -s $DOTFILES_DIR/oh-my-zsh/config.zsh $HOME/.oh-my-zsh/custom/config.zsh
+# sudo chsh -s `which zsh` $USER
 
 echo "nvim"
 # install
@@ -32,3 +38,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 
 echo "nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+source $HOME/.bashrc
+nvm install --lts
+npm install -g bs-platform
